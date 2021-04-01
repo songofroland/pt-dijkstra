4377644813d4f6702839292f5271383010edd74d
Jcob Chodubski <jchodubski@pm.me>
2021-04-01 17:54:17 +0200

    Added test

--- src/test/parsing.test.ts
+++ src/test/parsing.test.ts
@@ -21,6 +21,7 @@ const empty: Graph = [];
 const squareGraphString = '\n   1  1 1\n\n    1 1 1\n1 1 1\n\n\n\n';
 const traingleGraphString = ' 1  1  1\n \n 1 1 \n  1 '
 const invalidGraphString = '11 1\n1 1 1\n1 1 1';
+const emptyGraphString = '\n\n  aasd \n ';
 
 test('isSquare', () => {
     expect(isSquare(squareGraph)).toBeTruthy();
@@ -39,6 +40,7 @@ test('isTriangle', () => {
 test('isValid', () => {
     expect(isValid(squareGraphString)).toBeTruthy();
     expect(isValid(invalidGraphString)).toBeFalsy();
+    expect(isValid(emptyGraphString)).toBeFalsy();
 })
 
 test('parseValidGraph', () => {
