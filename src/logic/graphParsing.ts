d41bdb4741b5f895170f6dae82c9dadd5c72ca22
Jcob Chodubski <jchodubski@pm.me>
2021-04-09 13:06:43 +0200

    Remove triangle graphs

--- src/logic/graphParsing.ts
+++ src/logic/graphParsing.ts
@@ -16,41 +16,10 @@ export function isSquare(graph: Graph) : boolean {
   return graph.every(row => row.length === squareSideLength);
 }
 
-export function isTriangle(graph: Graph): boolean {
-  let correctRowLenght = graph.length;
-  for (const graphRow of graph) {
-    if (correctRowLenght === 0) break;
-    if (graphRow.length !== correctRowLenght) return false;
-    correctRowLenght--;
-  }
-  return true;
-}
 
 export function isValid(text: string): boolean {
   const graph = toMatrix(text);
-  if (!graph.length) return false;
-  return isSquare(graph) || isTriangle(graph);
-}
-
-function mirrorDiagonally(x: number, y: number, squareSide: number):
-  [number, number]
-{
-  const distanceFromSide = squareSide - y - 1;
-  const distanceFromBottom = squareSide - x - 1;
-  return [distanceFromBottom, distanceFromSide];
-}
-
-function squareGraph(graph: Graph): Graph {
-  const squareSide = graph.length;
-  for (let x = 0; x < squareSide; x++) {
-    for (let y = 0; y < squareSide; y++ ) {
-      if (graph[x][y] === undefined) {
-        const [x_m, y_m] = mirrorDiagonally(x, y, squareSide);
-        graph[x][y] = graph[x_m][y_m];
-      }
-    }
-  }
-  return graph;
+  return !!graph.length && isSquare(graph);
 }
 
 /**
@@ -59,6 +28,5 @@ function squareGraph(graph: Graph): Graph {
  */
 export function parseValidGraph(text: string): Graph {
   const graph = toMatrix(text);
-  if (isTriangle(graph)) return squareGraph(graph)
   return graph;
 }
--- src/test/parsing.test.ts
+++ src/test/parsing.test.ts
@@ -1,4 +1,4 @@
-import { isSquare, isTriangle, isValid, parseValidGraph } from '../logic/graphParsing';
+import { isSquare, isValid, parseValidGraph } from '../logic/graphParsing';
 import { Graph } from '../logic/commonInterfaces';
 
 const squareGraph = [
@@ -11,31 +11,17 @@ const invalidGraph = [
     [],
     []
 ];
-const traingleGraph = [
-    [1, 1, 1],
-    [1, 1],
-    [1]
-];
 const empty: Graph = [];
 
 const squareGraphString = '\n   1  1 1\n\n    1 1 1\n1 1 1\n\n\n\n';
-const traingleGraphString = ' 1  1  1\n \n 1 1 \n  1 '
 const invalidGraphString = '11 1\n1 1 1\n1 1 1';
 const emptyGraphString = '\n\n  aasd \n ';
 
 test('isSquare', () => {
     expect(isSquare(squareGraph)).toBeTruthy();
     expect(isSquare(invalidGraph)).toBeFalsy();
-    expect(isSquare(traingleGraph)).toBeFalsy();
     expect(isSquare(empty)).toBeTruthy();
 });
-
-test('isTriangle', () => {
-    expect(isTriangle(traingleGraph)).toBeTruthy();
-    expect(isTriangle(squareGraph)).toBeFalsy();
-    expect(isTriangle(invalidGraph)).toBeFalsy();
-    expect(isTriangle(empty)).toBeTruthy();
-})
 
 test('isValid', () => {
     expect(isValid(squareGraphString)).toBeTruthy();
@@ -45,5 +31,4 @@ test('isValid', () => {
 
 test('parseValidGraph', () => {
     expect(parseValidGraph(squareGraphString)).toStrictEqual(squareGraph);
-    expect(parseValidGraph(traingleGraphString)).toStrictEqual(squareGraph);
 })
