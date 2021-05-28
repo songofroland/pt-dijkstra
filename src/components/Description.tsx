ed99b92503d2d2dd5b78c07936978b66e5f994d7
Jcob Chodubski <jchodubski@pm.me>
2021-05-28 17:32:10 +0200

    Css upgrade & description placeholder (#14)
    * Css grid
    
    * EOF
    
    * Review changes

---
+++ src/components/Description.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Description() {
+  return <>
+    <div className="description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
+      Quam eveniet placeat mollitia unde iusto labore molestiae,<br/>
+      blanditiis impedit aperiam veniam non, aliquid assumenda,<br/>
+      eaque libero eius ab architecto! Ipsum, veniam.<br/>
+    </div>
+  </>;
+}
