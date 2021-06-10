9281eb8652f6ab977ff03adb781a86081ece6b08
Jcob Chodubski <jchodubski@pm.me>
2021-06-10 14:30:14 +0200

    Unify app look across browsers (#18)
    * Unified CSS styling across browsers
    
    * Added max-width to description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,9 +4,9 @@ export default function Description() {
   return <>
     <div className="description">
         Dijkstra&apos;s algorithm to find the shortest path between a and b.
-        It picks the unvisited vertex with the lowest distance,<br/>calculates 
+        It picks the unvisited vertex with the lowest distance, calculates 
         the distance through it to each unvisited neighbor, and updates the 
-        neighbor&apos;s distance if smaller.<br/> 
+        neighbor&apos;s distance if smaller.
         Mark visited (set to blue) when done with neighbors. 
     </div>
   </>;
